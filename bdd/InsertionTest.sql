439de19a60cc5f6a95ac97d34c982ff1615fedb6
Aboubakr <aboubakr.zennir@gmail.com>
2022-11-18 20:05:16 +0100

    new script BDD

--- bdd/InsertionTest.sql
+++ bdd/InsertionTest.sql
@@ -1,24 +1,17 @@
-INSERT INTO `taille` (`idTaille`, `specification`) VALUES ('1', 'Grand taille');
+INSERT INTO `administrateur_vente` (`idAdmin`, `login`, `pwd`) VALUES ('1', 'laurent', '1234');
+INSERT INTO `facture` (`idFacture`) VALUES ('1');
+INSERT INTO `qualite` (`idQualite`, `nomQualite`) VALUES ('1', 'qualite1'), ('2', 'qualite2');
+INSERT INTO `taille` (`idTaille`, `specification`) VALUES ('1', 'Grande taille');
+INSERT INTO `espece` (`idEspece`, `nomScientifiqueEspece`, `nomCommunEspece`) VALUES ('1', 'Baleine Bleue', 'Baleine');
+INSERT INTO `bateau` (`idBateau`, `nomBateau`) VALUES ('1', 'Aviso');
+INSERT INTO `peche` (`idBateau`, `datePeche`) VALUES ('1', '2022-11-18 19:45:22');
+INSERT INTO `directeur_vente` (`idDirecteur`, `nomDirecteur`) VALUES ('1', 'Paul');
+INSERT INTO `presentation` (`idPresentation`, `libellePr`) VALUES ('1', 'Presentation1');
+INSERT INTO `bac` (`idBac`, `tare`) VALUES ('1', 'Tare1');
+INSERT INTO `acheteur` (`idAcheteur`, `login`, `pwd`, `raisonSocialeEntreprise`, `adresse`, `ville`, `codePostal`, `numHabilitation`) VALUES ('1', 'eric', '1234', 'SAPMER', '6 Rue de la peche', 'Le Gardon', '30350', 'Aucun');
+INSERT INTO `lot` (`idLot`, `idBateau`, `datePeche`, `idEspece`, `idTaille`, `idPresentation`, `idBac`, `idAcheteur`, `idQualite`, `idAdmin`, `idDirecteur`, `idFacture`, `poidsBrutLot`, `prixPlancher`, `prixDepart`, `prixEnchereMax`, `dateEnchere`, `heureDebutEnchere`, `codeEtat`) VALUES ('1', '1', '2022-11-18 19:45:22', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1400', '650', '800', '1100', '2022-11-18 19:52:17', '2022-11-18 21:52:17', 'A');
+INSERT INTO `historique` (`dateEnchere`) VALUES ('2022-11-18 19:53:52');
+INSERT INTO `historique` (`dateEnchere`) VALUES ('2022-11-18 19:57:49');
+INSERT INTO `encherir` (`idLot`, `idBateau`, `datePeche`, `dateEnchere`, `idAcheteur`, `prixEnchere`) VALUES ('1', '1', '2022-11-18 19:45:22', '2022-11-18 19:53:52', '1', '1200');
+INSERT INTO `encherir` (`idLot`, `idBateau`, `datePeche`, `dateEnchere`, `idAcheteur`, `prixEnchere`) VALUES ('1', '1', '2022-11-18 19:45:22', '2022-11-18 19:57:49', '1', '1300');
 
-
-INSERT INTO `bac` (`idBac`, `tare`) VALUES ('1', '6');
-
-INSERT INTO `bateau` (`idBateau`) VALUES ('1');
-
-INSERT INTO `espece` (`idEspece`) VALUES ('1');
-
-INSERT INTO `qualite` (`idQualite`) VALUES ('1');
-
-INSERT INTO `presentation` (`idPresentation`) VALUES ('1');
-
-
-INSERT INTO `acheteur` (`idAcheteur`, `login`, `pwd`, `raisonSocialeEntreprise`, `adresse`, `ville`, `codePostal`, `numHabilitation`) VALUES ('1', 'ac1', 'jhjh765', 'AQUALANDE', '5 Rue des poissons', 'Cohade', '43000', 'CP67612876');
-
-
-INSERT INTO `peche` (`idBateau`, `datePeche`) VALUES ('1', '2022-10-14 10:38:17');
-
-
-INSERT INTO `lot` (`idLot`, `idBateau`, `datePeche`, `idEspece`, `idTaille`, `idPresentation`, `idBac`, `idAcheteur`, `idQualite`, `poidsBrutLot`, `prixPlancher`, `prixDepart`, `prixEnchereMax`, `dateEnchere`, `heureDebutEnchere`, `codeEtat`, `idFacture`) VALUES ('1', '1', '2022-10-14 10:38:17', '1', '1', '1', '1', '1', '1', '3123', '1231231', '123213123', '123123131221', '2022-10-17 10:50:04', '2022-10-18 10:50:04', '123DS', '128');
-
-
-INSERT INTO `poster` (`idAcheteur`, `idBateau`, `datePeche`, `idLot`, `prixEnchere`, `heureEnchere`) VALUES ('1', '1', '2022-10-14 10:38:17', '1', '42421', '2022-10-17 10:50:55');
--- bdd/Script-BDD-Criee.sql
+++ bdd/Script-BDD-Criee.sql
@@ -4,9 +4,46 @@ CREATE DATABASE BddCriee ;
 
 USE BddCriee ;
 
+
+CREATE TABLE ADMINISTRATEUR_VENTE(
+   idAdmin INT(10) PRIMARY KEY,
+   login VARCHAR(50),
+   pwd VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE FACTURE(
+   idFacture INT(10) PRIMARY KEY
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE QUALITE(
+   idQualite INT(10) PRIMARY KEY,
+   nomQualite VARCHAR(10)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE TAILLE(
    idTaille INT(10) PRIMARY KEY,
    specification VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE ESPECE(
+   idEspece INT(10) PRIMARY KEY,
+   nomScientifiqueEspece VARCHAR(30),
+   nomCommunEspece VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE BATEAU(
+   idBateau INT(10) PRIMARY KEY,
+   nomBateau VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE DIRECTEUR_VENTE(
+   idDirecteur INT(10) PRIMARY KEY,
+   nomDirecteur VARCHAR(30)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE PRESENTATION(
+   idPresentation VARCHAR(50) PRIMARY KEY,
+   libellePr VARCHAR(30)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE BAC(
@@ -14,22 +51,9 @@ CREATE TABLE BAC(
    tare VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE BATEAU(
-   idBateau INT(10) PRIMARY KEY
+CREATE TABLE HISTORIQUE(
+   dateEnchere DATETIME PRIMARY KEY
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE ESPECE(
-   idEspece INT(10) PRIMARY KEY
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE QUALITE(
-   idQualite INT(10) PRIMARY KEY
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE PRESENTATION(
-   idPresentation VARCHAR(50) PRIMARY KEY
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE ACHETEUR(
    idAcheteur INT(10) PRIMARY KEY,
@@ -49,7 +73,6 @@ CREATE TABLE PECHE(
    FOREIGN KEY (idBateau) REFERENCES BATEAU(idBateau)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE LOT(
    idLot INT(10),
    idBateau INT(10),
@@ -60,6 +83,9 @@ CREATE TABLE LOT(
    idBac INT(10),
    idAcheteur INT(10),
    idQualite INT(10),
+   idAdmin INT(10),
+   idDirecteur INT(10),
+   idFacture INT(10),
    poidsBrutLot VARCHAR(50),
    prixPlancher VARCHAR(50),
    prixDepart VARCHAR(50),
@@ -67,7 +93,6 @@ CREATE TABLE LOT(
    dateEnchere DATETIME,
    heureDebutEnchere DATETIME,
    codeEtat VARCHAR(10),
-   idFacture INT(10),
    PRIMARY KEY(idBateau, datePeche, idLot),
    FOREIGN KEY(idQualite) REFERENCES QUALITE(idQualite),
    FOREIGN KEY(idTaille) REFERENCES TAILLE(idTaille),
@@ -75,18 +100,22 @@ CREATE TABLE LOT(
    FOREIGN KEY(idBateau, datePeche) REFERENCES PECHE(idBateau, datePeche),
    FOREIGN KEY(idBac) REFERENCES BAC(idBac),
    FOREIGN KEY(idAcheteur) REFERENCES ACHETEUR(idAcheteur),
-   FOREIGN KEY(idPresentation) REFERENCES PRESENTATION(idPresentation)
+   FOREIGN KEY(idPresentation) REFERENCES PRESENTATION(idPresentation),
+   FOREIGN KEY(idFacture) REFERENCES FACTURE(idFacture),
+   FOREIGN KEY(idAdmin) REFERENCES ADMINISTRATEUR_VENTE(idAdmin),
+   FOREIGN KEY(idDirecteur) REFERENCES DIRECTEUR_VENTE(idDirecteur)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE POSTER(
-   idAcheteur INT(10),
+CREATE TABLE ENCHERIR(
+   idLot INT(10),
    idBateau INT(10),
    datePeche DATETIME,
-   idLot INT(10),
-   prixEnchere VARCHAR(50),
-   heureEnchere DATETIME,
-   PRIMARY KEY(idBateau, datePeche, idLot, idAcheteur),
+   dateEnchere DATETIME,
+   idAcheteur INT(10),
+   prixEnchere VARCHAR(10),
+   PRIMARY KEY(dateEnchere,idAcheteur,idBateau, datePeche, idLot),
    FOREIGN KEY(idBateau, datePeche, idLot) REFERENCES LOT(idBateau, datePeche, idLot),
+   FOREIGN KEY(dateEnchere) REFERENCES HISTORIQUE(dateEnchere),
    FOREIGN KEY(idAcheteur) REFERENCES ACHETEUR(idAcheteur)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
