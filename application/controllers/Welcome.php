4fa700f37353cbb8499ac3a9d9cefd3f50dea0d8
Wassim67_ <wjallabi@numerique-educatif.fr>
2022-12-16 09:28:22 +0100

    creation de toutes les variables d'inscription

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -50,5 +50,43 @@ public function url($id){
 }
 
 
+public function inscriptionAcheteur() 
+	{
+	   $this->form_validation->set_rules('nomAcheteur', '"Le nom"', 'trim|required');//|xss_clean');
+	   $this->form_validation->set_rules('prenomAcheteur', '"Le prenom"', 'trim|required');//|xss_clean');
+	   $this->form_validation->set_rules('mailAcheteur', '"L\'adresse email"', 'trim|required|valid_email');//|xss_clean');//is_unique[users.email]
+	   $this->form_validation->set_rules('mdpPremierAcheteur', '"Le Mot de passe"', 'trim|required');//|xss_clean');//is_unique[users.email]  
+	/*
+	Supprime les espaces au début et à la fin de la chaîne
+	Vérifie que la chaîne résultante n'est pas vide
+	Vérifie qu'il s'agit d'une adresse e-mail valide
+	*/	
+
+		if ($this->form_validation->run() == FALSE)
+		{ 
+			//$this->session->set_flashdata('fail','Données non enregistré');
+        } 
+        else
+		{ 
+		$nomAcheteur = strip_tags($this->input->post('nomAcheteur'));
+		$prenomAcheteur = strip_tags($this->input->post('prenomAcheteur'));
+		$mailAcheteur = strip_tags($this->input->post('mailAcheteur'));
+		$mdpAcheteur = password_hash($this->input->post('mdpPremierAcheteur', PASSWORD_DEFAULT)); //pour crypter les mdps pour l'instant (car pas java script)
+		$loginAcheteur = strip_tags($this->input->post('loginAcheteur'));
+		$raisonSocialEntrepriseAcheteur = strip_tags($this->input->post('raisonSocialEntreprise'));
+		$villeAcheteur = strip_tags($this->input->post('villeAcheteur'));
+		$adresseAcheteur = strip_tags($this->input->post('adresseAcheteur'));
+		$codePostalAcheteur = strip_tags($this->input->post('codePostalAcheteur'));
+		$numHabilitation = strip_tags($this->input->post('numHabilitation'));
+
+
+		//$data['resultat']=$this->requetes->setUtilisateur($nomU,$prenomU,$mdpU,$mailU,$numStatut);
+		//$this->session->set_flashdata('succes','Données enregistrées, merci ! Vous pouvez désormais vous connecter');
+		//redirect(base_url('inscription'));
+
+        }	 
+ 	 
+	}	
+
 
 }
--- application/views/inscription.php
+++ application/views/inscription.php
@@ -12,42 +12,63 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php
     echo "<br>";
-    echo form_open('',array('method'=>'post'));
+    echo form_open('welcome/inscriptionAcheteur',array('method'=>'post'));
 ?>
 
 	<h1>Formulaire d'inscription</h1>
 
 	<?php
     echo "<br>";
-	$nomU= array('name'=>'Nom utilisateur','id'=>'nom Acheteur','placeholder'=>'Votre nom d\'utilisateur ','value'=>set_value('nomU'));
-    echo form_input($nomU);
+	$nomAcheteur= array('name'=>'nomAcheteur','id'=>'nom Acheteur','placeholder'=>'Votre nom de famille ','value'=>set_value('nomAcheteur'));
+    echo form_input($nomAcheteur);
     echo "<br>";
     echo "<br>";
-	$prenomU= array('name'=>'prenomU','id'=>'prenomU','placeholder'=>'Votre prenom','value'=>set_value('prenomU'));
-    echo form_input($prenomU);
+	$prenomAcheteur= array('name'=>'prenomAcheteur','id'=>'prenomAcheteur','placeholder'=>'Votre prenom','value'=>set_value('prenomAcheteur'));
+    echo form_input($prenomAcheteur);
     echo "<br>";
     echo "<br>";
-    $mailU= array('name'=>'mailU','id'=>'mailU','placeholder'=>'Entrer votre adresse mail','value'=>set_value('mailU'));
-    echo form_input($mailU);
+    $mailAcheteur= array('name'=>'mailAcheteur','id'=>'mailAcheteur','placeholder'=>'Entrer votre adresse mail','value'=>set_value('mailAcheteur'));
+    echo form_input($mailAcheteur);
     echo "<br>";
     echo "<br>";
+    $loginAcheteur= array('name'=>'loginAcheteur','id'=>'loginAcheteur','placeholder'=>'Votre nom d\'utilisateur','value'=>set_value('loginAcheteur'));
+    echo form_input($loginAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $mdpPremierAcheteur= array('name'=>'mdpPremierAcheteur','id'=>'mdpPremierAcheteur','placeholder'=>'Entrer votre mot de passe','value'=>set_value('mdpPremierAcheteur'));
+    echo form_input($mdpPremierAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $mdpConfirmeAcheteur= array('name'=>'mdpConfirmeAcheteur','id'=>'mdpConfirmeAcheteur','placeholder'=>'retaper votre mot de passe','value'=>set_value('mdpConfirmeAcheteur'));
+    echo form_input($mdpConfirmeAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $raisonSocialEntrepriseAcheteur= array('name'=>'raisonSocialEntreprise','id'=>'raisonSocialEntreprise','placeholder'=>'Raison social de l\'entreprise ','value'=>set_value('raisonSocialEntreprise'));
+    echo form_input($raisonSocialEntrepriseAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $villeAcheteur= array('name'=>'villeAcheteur','id'=>'villeAcheteur','placeholder'=>'Votre ville','value'=>set_value('villeAcheteur'));
+    echo form_input($villeAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $adresseAcheteur= array('name'=>'adresseAcheteur','id'=>'adresseAcheteur','placeholder'=>'Votre adresse ','value'=>set_value('adresseAcheteur'));
+    echo form_input($adresseAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $codePostalAcheteur= array('name'=>'codePostalAcheteur','id'=>'codePostalAcheteur','placeholder'=>'Votre code postal ','value'=>set_value('codePostalAcheteur'));
+    echo form_input($codePostalAcheteur);
+    echo "<br>";
+    echo "<br>";
+    $numHabilitation= array('name'=>'numHabilitation','id'=>'numHabilitation','placeholder'=>'Le numéro d\'habilitation ','value'=>set_value('numHabilitation'));
+    echo form_input($numHabilitation);
 
-	$mdpPremier= array('name'=>'mdpPremier','id'=>'mdpPremier','placeholder'=>'Entrer votre mot de passe','value'=>set_value('mdpPremier'));
-    echo form_input($mdpPremier);
-
     echo "<br>";
     echo "<br>";
-    $mdpConfirme= array('name'=>'mdpConfirme','id'=>'mdpConfirme','placeholder'=>'retaper votre mot de passe','value'=>set_value('mdpConfirme'));
-    echo form_input($mdpConfirme);
-
-    echo "<br>";
-    echo "<br>";
-	echo form_submit('envoi', 'Valider');
+    echo form_submit('envoi', 'Valider');
 	?>
     
 
 </center>
-
 	<?php
     echo form_close();
 ?>
