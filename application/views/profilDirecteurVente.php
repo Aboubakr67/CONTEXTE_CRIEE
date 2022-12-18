717afadaa2dafd63a5167a9e144018cd06f86d30
Wassim67_ <wjallabi@numerique-educatif.fr>
2022-12-18 15:08:24 +0100

    mise en place de la connexion et model utilitaire

---
+++ application/views/profilDirecteurVente.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if(empty($_SESSION['login'])){
+    header('Location: connexion');
+  }
+
+  echo "Bonjour !"; 
+  echo $_SESSION['login'];
+?>
