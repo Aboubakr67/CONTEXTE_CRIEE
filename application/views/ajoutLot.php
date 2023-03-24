7f9b4a13c44bde311b9e090864226d964f58f90c
juliano <julianojuliano973@gmail.com>
2023-03-24 10:48:57 +0100

    test

--- application/views/ajoutLot.php
+++ application/views/ajoutLot.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <?php
 if (empty($_SESSION['login'])) {
   if ($_SESSION['login'] != 'laurent')
-    header('Location: connexion');
+    header('Location: connexion'); // 
 }
 
 $DateAndTime = date('Y-m-d', time());
